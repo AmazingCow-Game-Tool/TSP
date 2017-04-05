c0d694384a57c5c167ebacfe255f0116cf268757
n2omatt <n2o.matt@gmail.com>
2017-04-04 21:51:11 -0300

    - Remove the old (wrong) dtor declaration.

--- include/ImageSorter/AreaImageSorter.h
+++ include/ImageSorter/AreaImageSorter.h
@@ -6,10 +6,6 @@
 class AreaImageSorter :
     public IImageSorter
 {
-    // CTOR / DTOR //
-public:
-    virtual ~IImageSorter();
-
     // Interface Methods //
 public:
     virtual int sort(const Image &img1, const Image &img2) const override
--- include/ImageSorter/MaxSideImageSorter.h
+++ include/ImageSorter/MaxSideImageSorter.h
@@ -8,10 +8,6 @@
 class MaxSideImageSorter :
     public IImageSorter
 {
-    // CTOR / DTOR //
-public:
-    virtual ~IImageSorter();
-
     // Interface Methods //
 public:
     virtual int sort(const Image &img1, const Image &img2) const override

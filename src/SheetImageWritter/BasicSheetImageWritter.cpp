ed62c005022645e08e49255be9bc6b07fed31db2
n2omatt <n2o.matt@gmail.com>
2017-04-15 01:36:54 -0300

    - Change the interface to pass the SheetWritterOptions.
    This will enable us to enhance and modify the needed
    information more easier.

--- src/SheetImageWritter/BasicSheetImageWritter.cpp
+++ src/SheetImageWritter/BasicSheetImageWritter.cpp
@@ -5,35 +5,29 @@
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// CTOR                                                                       //
-////////////////////////////////////////////////////////////////////////////////
-BasicSheetImageWritter::BasicSheetImageWritter(const QString &outputPath) :
-    m_outputPath(outputPath)
-{
-    //Empty...
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
 // Interface Methods                                                          //
 ////////////////////////////////////////////////////////////////////////////////
-void BasicSheetImageWritter::write(
-    const QVector<Image> &images,
-    const QVector<QRect> &rects,
-    const QSize          &size) const
+void BasicSheetImageWritter::write(const SheetWritterOptions &options) const
 {
-    QImage   sheet_image(size, QImage::Format_RGBA8888);
+    //Put all the images into the final image.
+    QImage   sheet_image(options.sheetSize, QImage::Format_RGBA8888);
     QPainter painter    (&sheet_image);
 
-    for(int i = 0; i < images.size(); ++i)
+    for(int i = 0; i < options.images.size(); ++i)
     {
-        const auto &src_image = images[i];
-        const auto &src_rect  = rects [i];
+        const auto &src_image = options.images[i];
+        const auto &src_rect  = options.rects [i];
 
         painter.drawImage(src_rect, src_image.getQImage());
     }
 
     painter.end();
 
-    sheet_image.save(m_outputPath);
+
+    //Save the image.
+    auto filename   = options.imageOutputFilename;
+    auto outpath    = options.outputPath;
+    auto final_path = QDir(outpath).filePath(filename);
+
+    sheet_image.save(final_path);
 }

d6e5770ea7a7ed224862848779d50b77cef1456e
n2omatt <n2omatt@amazingcow.com>
2018-03-07 19:20:16 -0300

    - Add more options.

--- include/Core/RunInfo.h
+++ include/Core/RunInfo.h
@@ -3,22 +3,38 @@
 // std
 #include <string>
 #include <vector>
+// AmazingCow Libs
+#include "acow/math_goodies.h"
 
 namespace TSP { namespace Core {
 
+//----------------------------------------------------------------------------//
+// Types                                                                      //
+//----------------------------------------------------------------------------//
 struct RunInfo
 {
+    // Directories.
     std::vector<std::string> dirs_Images;
+    std::vector<std::string> dirs_ImagesAllowedExtensions;
     std::string              dir_Output;
 
-    std::string sheet_ImageName;
-    std::string sheet_DataName;
+    // Image Info.
+    std::string      sheet_ImageName;
+    std::string      sheet_ImageFormat;
+    bool             sheet_ImageDebug;
+    acow::math::Size sheet_SizeMin;
+    acow::math::Size sheet_SizeMax;
+    bool             sheet_ForcePOT;
+    bool             sheet_ForceSquare;
 
-    std::string sheet_ImageFormat;
+    // Data Info.
+    std::string sheet_DataName;
     std::string sheet_DataFormat;
 
-    bool debug_Image;
+    // Pack.
+    std::string pack_SortType;
 
+    // GUI.
     bool gui_Enabled;
 
 }; // struct RunInfo

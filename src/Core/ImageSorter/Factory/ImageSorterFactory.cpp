c6edae60d63d77f689868d98942dd482bf6f3c2e
n2omatt <n2omatt@amazingcow.com>
2018-03-08 18:55:57 -0300

    [BUGFIX] We were returning nullptrs... :x

--- src/Core/ImageSorter/Factory/ImageSorterFactory.cpp
+++ src/Core/ImageSorter/Factory/ImageSorterFactory.cpp
@@ -20,13 +20,14 @@ using namespace Core;
 IImageSorter::UPtr
 ImageSorterFactory::CreateUnique(const std::string &name) noexcept
 {
-    COREASSERT_ASSERT(name.empty(), "name can't be empty.");
+    COREASSERT_ASSERT(!name.empty(), "name can't be empty.");
     auto lower_name = CoreString::ToLower(name);
 
-    if     (lower_name == "area"   ) return AreaImageSorter   ::UPtr();
-    else if(lower_name == "height" ) return HeightImageSorter ::UPtr();
-    else if(lower_name == "maxside") return MaxSideImageSorter::UPtr();
-    else if(lower_name == "width"  ) return WidthImageSorter  ::UPtr();
+    if     (lower_name == "area"   ) return acow::make_unique<AreaImageSorter   >();
+    else if(lower_name == "height" ) return acow::make_unique<HeightImageSorter >();
+    else if(lower_name == "maxside") return acow::make_unique<MaxSideImageSorter>();
+    else if(lower_name == "width"  ) return acow::make_unique<WidthImageSorter  >();
 
     COREASSERT_ASSERT(false, "Invalid image sorter - name: (%s)", name.c_str());
+    return nullptr; // Just to make compiler happy...
 }

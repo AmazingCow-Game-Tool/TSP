9de60ec1486089d1d940ef9e511307e983392947
n2omatt <n2omatt@amazingcow.com>
2018-03-08 18:58:03 -0300

    - Add a umbrella header for Core stuff.

---
+++ include/Core/Core.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#include "SheetRectWriter/SheetRectWriters.h"
+#include "ImageLoader/ImageLoaders.h"
+#include "PackingStrategy/PackingStrategies.h"
+#include "SheetImageWriter/SheetImageWriters.h"
+#include "ImageFinder/ImageFinders.h"
+#include "ImageSorter/ImageSorters.h"
+#include "Image/Images.h"
+#include "PackerConstraint/PackerConstraints.h"
+#include "RunInfo.h"
--- include/TSP.h
+++ include/TSP.h
@@ -1,12 +1,4 @@
 #pragma once
 
-#include "include/Core/SheetRectWriter/SheetRectWriters.h"
-#include "include/Core/ImageLoader/ImageLoaders.h"
-#include "include/Core/PackingStrategy/PackingStrategies.h"
-#include "include/Core/SheetImageWriter/SheetImageWriters.h"
-#include "include/Core/ImageFinder/ImageFinders.h"
-#include "include/Core/ImageSorter/ImageSorters.h"
-#include "include/Core/Image/Images.h"
-#include "include/Core/PackerConstraint/PackerConstraints.h"
-#include "include/Core/RunInfo.h"
+#include "include/Core/Core.h"
 #include "include/UI/UI.h"
